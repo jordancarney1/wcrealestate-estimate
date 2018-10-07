e9c9815ec9e321c02b85c639e7460368d309aa13
Jordan Carney <jordan@jordancarney.com>
2018-10-07 16:45:26 -0400

    shorthand express invokation.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
-const express = require('express')
+const app = require('express')()
 const bodyParser = require('body-parser')
 const routes = require('./routes')
-const app = express()
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
