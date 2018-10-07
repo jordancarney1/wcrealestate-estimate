7ea07d4c72663516987ba44aa08343fd50dce0ab
Jordan Carney <jordan@jordancarney.com>
2018-10-07 18:31:12 -0400

    modularlize routing.

--- routes/index.js
+++ routes/index.js
@@ -1,103 +1,12 @@
 const path = require('path')
-const routes = require('express').Router()
-const axios = require('axios')
-const parseXMLString = require('xml2js').parseString
-const apis = require('../apis')
-const sendEstimate = require('../email')
+const router = module.exports = require('express').Router()
+const routes = [
+  require('./estimate-request'),
+]
+
+routes.forEach(route => router.use(route))
 
 // Temporary route - remove later
-routes.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../index.html')))
+router.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../index.html')))
 
-routes.get('/bundle.js', (req, res) => res.sendFile(path.join(__dirname + '/../bundle.js')));
-
-routes.post('/estimate-request', (req, res) => {
-  // TODO: Implment pseudo
-  // if (
-  //   req.headers.origin !== 'deployed address' ||
-  //   req.headers.referer !== 'deployed address'
-  // ) { console.error('Not from trusted requestor') }
-
-  // TODO: Implement pseudo
-  // if (
-  //   !req.body
-  //   !req.body.emailAddress
-  //   !req.body.streetAddress
-  //   !req.body.city
-  //   !req.body.state
-  // ) { console.error('Not a valid request') }
-
-
-  const {
-    address,
-    email: requestEmail,
-  } = req.body
-  
-  // TODO: Need validations
-  const [streetAddress, city, state, zip]= address.split(',').map(val => val.trim())
-
-  // Error handling!
-  // if no amount - what to send back to FE? Error with email to Whitney?
-  // if zillow error - what to send back to FE? Error with email to Whitney for followup?
-  const handleZillowResponse = (zillowResponse) => {
-    let jsonResponse
-    if (
-      zillowResponse &&
-      typeof zillowResponse === 'object' &&
-      zillowResponse.status === 200 &&
-      zillowResponse.data &&
-      typeof zillowResponse.data === 'string'
-    ) {
-      parseXMLString(zillowResponse.data, { explicitArray: false }, (err, result) => jsonResponse = JSON.parse(JSON.stringify(result)))
-    }
-
-    if (
-      jsonResponse &&
-      jsonResponse["SearchResults:searchresults"] &&
-      jsonResponse["SearchResults:searchresults"].message &&
-      jsonResponse["SearchResults:searchresults"].message.code === '0'
-    ) {
-      const {
-        zpid: zillowPropertyId,
-        zestimate: {
-          amount: {
-            _: zestimate,
-          },
-        },
-        links: {
-          comparables: comparablesLink,
-          homedetails: homeDetailsLink,
-        },
-      } = jsonResponse["SearchResults:searchresults"].response.results.result
-      const formattedEstimate = parseInt(zestimate).toLocaleString('en-US')
-
-      const dataToSend = {
-        address,
-        requestEmail,
-        comparablesLink,
-        homeDetailsLink,
-        formattedEstimate,
-      }
-
-      sendEstimate(dataToSend)
-
-    } else {
-      console.error('Did not get a valid response from Zillow', zillowResponse)
-    }
-  }
-
-  axios({
-    method: 'get',
-    url: apis.zillow,
-    params: {
-      ['zws-id']: process.env.ZWSID,
-      address: streetAddress,
-      citystatezip: city+','+state+(zip ? ','+zip : ''),
-    },
-    responseType: 'text',
-  })
-    .then(handleZillowResponse)
-    .catch(err => console.log(err))
-
-})
-
-module.exports = routes
+router.get('/bundle.js', (req, res) => res.sendFile(path.join(__dirname + '/../bundle.js')));
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const app = require('express')()
 const bodyParser = require('body-parser')
-const routes = require('./routes')
+const router = require('./routes')
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/', routes)
+app.use(router)
 
 app.listen(port,
   () => console.log(`Whitney Carney - Free Estimate Service listening on port ${port}!`)
