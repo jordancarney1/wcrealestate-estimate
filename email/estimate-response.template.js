98769b4ac0eb4fa4533649da4e711615a7768cca
Jordan Carney <jordan@jordancarney.com>
2018-10-08 19:06:05 -0400

    html email template updates

--- email/estimate-response.template.js
+++ email/estimate-response.template.js
@@ -1,7 +1,6 @@
 module.exports = function(data) {
   const {
     address,
-    formattedZestimate,
     requestEmail,
     formattedZestimateRangeLow,
     formattedZestimateRangeHigh,
@@ -15,18 +14,35 @@ module.exports = function(data) {
 
     You can see the instant valuations for your home below:
 
-    <b>Eppraisal</b>
-    Between <b>$166,515</b> and <b>$225,285</b>
-
-    <b>Zillow Zestimate</b>
-    Between <b>$${formattedZestimateRangeLow}</b> and <b>$${formattedZestimateRangeHigh}</b>
+    Zillow Zestimate</b>
+    Between $${formattedZestimateRangeLow} and $${formattedZestimateRangeHigh}
     
     These online valuations aren't always as accurate as they could be, so I can create a custom report for you. Have you recently remodeled or know of anything that might affect your home's value?
 
     Regards,
 
     ${process.env.MAIN_NAME_FULL}
+    ${process.env.SIGNATURE_TAG_LINE}
     ${process.env.MAIN_PHONE}
     ${process.env.MAIN_EMAIL}`,
+    html: `
+      <html>
+      Hi there,<br />
+      <br />
+      You can see the instant valuations for your home below:<br />
+      <br />
+      <b>Zillow Zestimate</b><br />
+      Between <b>$${formattedZestimateRangeLow}</b> and <b>$${formattedZestimateRangeHigh}</b><br />
+      <br />
+      These online valuations aren't always as accurate as they could be, so I can create a custom report for you. Have you recently remodeled or know of anything that might affect your home's value?
+      <br /><br />
+      Regards,<br/>
+      <br/>
+      ${process.env.MAIN_NAME_FULL}<br/>
+      ${process.env.SIGNATURE_TAG_LINE}<br/>
+      <a href="tel: ${process.env.MAIN_PHONE}">${process.env.MAIN_PHONE}</a><br/>
+      ${process.env.MAIN_EMAIL}<br/>
+      </html>
+    `,
   }
 }
