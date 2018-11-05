85d7b5ad009e7fccd9995582fd9d481960c09806
Jordan Carney <jordan@jordancarney.com>
2018-11-04 20:25:04 -0500

    temporary prod logging

--- apis/zillow.js
+++ apis/zillow.js
@@ -15,6 +15,11 @@ module.exports = {
     responseType: 'text',
   }),
   handleResponse: (zillowResponse) => {
+    console.log('============')
+    console.log(zillowResponse)
+    console.log('============')
+    console.log(zillowResponse.data)
+    console.log('============')
     let jsonResponse
     if (
       zillowResponse &&
